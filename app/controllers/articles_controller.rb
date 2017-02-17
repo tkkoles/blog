69c358d3620b3beea3276f2e128f535e1a096c1b
Tomasz Oleś <tkkoles@gmail.com>
2017-02-17 23:15:18 +0100

    Lifting

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 
 class ArticlesController < ApplicationController
  
-  http_basic_authenticate_with name: "tomek", password: "secret", except: [:index, :show]
+  http_basic_authenticate_with name: "tomek", password: "secret", except: [:index, :show, :edit]
  
   def index
     @articles = Article.all
